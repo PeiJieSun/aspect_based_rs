22bb8151e36832cadf99151d6ae1b0b28d133509
Peijie Sun <sun.hfut@gmail.com>
2020-04-16 09:22:46 +0000

    amazon digital fm 01 commit

---
+++ drive/My Drive/task/aspect_based_rs/src/amazon_digital/fm/config_fm.py
@@ -0,0 +1,22 @@
+num_users = 5541
+num_items = 3568
+
+avg_rating = 4.2230
+
+embedding_dim = 32
+lr_abae = 1.0
+lr_rating = 1.0
+
+lr = 2e-3
+
+num_workers = 0
+
+batch_size = 128
+train_epochs = 20
+weight_decay = 1e-3
+
+root_path = '/content/drive/My Drive/task/aspect_based_rs'
+data_name = 'amazon_videos'
+target_path = '%s/data/%s' % (root_path, data_name)
+out_path = '%s/out/%s' % (root_path, data_name)
+model_path = '%s/out/model' % root_path
