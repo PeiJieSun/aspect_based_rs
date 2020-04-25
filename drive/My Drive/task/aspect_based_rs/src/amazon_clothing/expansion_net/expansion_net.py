8e900676be463b47bf0f5ca067de0cec730f55d4
Peijie Sun <sun.hfut@gmail.com>
2020-04-25 03:15:12 +0000

    amazon clothing expansion_net 17 commit

--- drive/My Drive/task/aspect_based_rs/src/amazon_clothing/expansion_net/expansion_net.py
+++ drive/My Drive/task/aspect_based_rs/src/amazon_clothing/expansion_net/expansion_net.py
@@ -32,7 +32,7 @@ class expansion_net(nn.Module):
         self.linear_3 = nn.Linear(conf.k+conf.word_dimension+conf.n, conf.k)
         self.linear_4 = nn.Linear(conf.n+conf.m, 1)
 
-        self.linear_5 = nn.Linear(conf.n+conf.m, conf.vocab_sz)
+        self.linear_5 = nn.Linear(conf.n, conf.vocab_sz)
 
         self.linear_6 = nn.Linear(conf.n, conf.vocab_sz)
 
@@ -64,7 +64,7 @@ class expansion_net(nn.Module):
 
         h_0 = (u_vector + v_vector).view(1, user.shape[0], conf.hidden_size) # (1 * 1, batch_size, hidden_size=n)
 
-        outputs, h_n = self.rnn(review_input_embed, h_0) # (seq_length, batch_size, hidden_size=n)
+        outputs, h_n = self.rnn(review_input_embed) # (seq_length, batch_size, hidden_size=n)
         review_output_embed = outputs.view(-1, outputs.size()[2])#(seq_length * batch_size, hidden_size=n)
         
         # calculate a2t
@@ -94,15 +94,15 @@ class expansion_net(nn.Module):
         a3t = torch.tanh(self.linear_3(torch.cat((sui.repeat(outputs.shape[0], 1), review_input_embed.view(-1, conf.word_dimension), review_output_embed), 1))) # (seq_length*batch_size, k)
 
         ############################### Pv(Wt) #########################################
-        PvWt = torch.tanh(self.linear_5(torch.cat([review_output_embed, a2t], 1))) # (seq_length*batch_size, vocab_sz)
-        #PvWt = torch.tanh(self.linear_5(torch.cat([review_output_embed], 1))) # (seq_length*batch_size, vocab_sz)
+        #PvWt = torch.tanh(self.linear_5(torch.cat([review_output_embed, a2t], 1))) # (seq_length*batch_size, vocab_sz)
+        PvWt = torch.tanh(self.linear_5(torch.cat([review_output_embed], 1))) # (seq_length*batch_size, vocab_sz)
 
         ############################### P(Wt) ######################################### 
         aspect_probit = torch.index_select(a3t, 1, review_aspect) * review_aspect_bool # (seq_length*batch_size, vocab_sz)
         #aspect_probit = F.log_softmax(aspect_probit, 1)
 
         #PvWt = torch.tanh(self.linear_6(review_output_embed))
-        Pwt = PvWt #+ aspect_probit
+        Pwt = PvWt + aspect_probit
         obj_loss = F.nll_loss(F.log_softmax(Pwt, 1), review_output.view(-1), reduction='mean')
 
         return obj_loss
