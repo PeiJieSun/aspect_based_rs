82cf9c6674b475e796f2a00420030173629388ab
Peijie Sun <sun.hfut@gmail.com>
2020-04-26 01:48:36 +0000

    amazon clothing expansion_net 30 commit

--- drive/My Drive/task/aspect_based_rs/src/amazon_clothing/expansion_net/expansion_net.py
+++ drive/My Drive/task/aspect_based_rs/src/amazon_clothing/expansion_net/expansion_net.py
@@ -47,8 +47,14 @@ class expansion_net(nn.Module):
         gamma_u = self.gamma_user_embedding(user) # (batch_size, m)
         gamma_i = self.gamma_item_embedding(item) # (batch_size, m)
 
+        gamma_u = F.normalize(gamma_u, dim=1)
+        gamma_i = F.normalize(gamma_i, dim=1)
+
         beta_u = self.beta_user_embedding(user) # (batch_size, k)
         beta_i = self.beta_item_embedding(item) # (batch_size, k)
+
+        beta_u = F.normalize(beta_u, dim=1)
+        beta_i = F.normalize(beta_i, dim=1)
 
         review_input_embed = self.word_embedding(review_input)# (seq_length, batch_size, word_dimension)
 
