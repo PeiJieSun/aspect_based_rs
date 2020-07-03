cadee78d1104a3067bf75160bed7a5ff39d7867a
Peijie Sun <sun.hfut@gmail.com>
2020-07-03 10:05:57 +0000

    amazon musical test ABAE+Rating Prediction AEAE_1e-4 reproducibility commit

---
+++ drive/My Drive/task/aspect_based_rs/src/amazon_videos_test/ncf/config_ncf.py
@@ -0,0 +1,26 @@
+weight_decay = 0.02
+learning_rate = 0.002
+
+gmf_embed_dim = 32
+mlp_embed_dim = 32
+
+mlp_dim_list = [64, 128, 64, 32]
+
+avg_rating = 4.0855
+
+num_users = 24301
+num_items = 24301
+
+batch_size = 256
+
+train_epochs = 20
+
+root_path = '/content/drive/My Drive/task/aspect_based_rs'
+data_name = 'amazon_musical_test'
+target_path = '%s/data/%s' % (root_path, data_name)
+out_path = '%s/out/%s' % (root_path, data_name)
+model_path = '%s/out/model' % root_path
+
+
+#### parameters for probe: PMF
+mf_dim = 32
